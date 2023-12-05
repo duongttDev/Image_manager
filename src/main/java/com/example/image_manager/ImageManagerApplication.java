476c0d8667435bce211410f0c0f969c70376d9ed
toanvmhe130559 <toanvmhe130559@fpt.edu.vn>
2023-12-05 19:42:31 +0700

    add Cors

--- src/main/java/com/example/image_manager/ImageManagerApplication.java
+++ src/main/java/com/example/image_manager/ImageManagerApplication.java
@@ -2,6 +2,9 @@ package com.example.image_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ImageManagerApplication {
@@ -10,4 +13,17 @@ public class ImageManagerApplication {
         SpringApplication.run(ImageManagerApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://192.168.0.105:8080/")
+                        //.allowedOrigins("http://localhost:8080") // Thay thế địa chỉ này bằng địa chỉ của frontend của bạn
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Content-Type", "Authorization");
+            }
+        };
+    }
 }
